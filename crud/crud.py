6a49a52b4ea6e3dd808ee005a50120079f8325fb
Michele Sciabarra <michele@sciabarra.com>
2018-11-20 21:31:09 +0100

    updated crud

--- crud/crud.py
+++ crud/crud.py
@@ -1,6 +1,5 @@
 import crud
 import rest
-
 db = "demodb"
 dtype = "contact"
 
@@ -9,17 +8,23 @@ def init(_db, _dtype):
   db = _db
   dtype = _dtype
 
-def delete(id):
-  a = id.split(":")
-  if len(a) == 1:
-    res = find(id)
-    if res["docs"]:
-      a = res["docs"][0]["id"].split(":")
-  params = {
-    "docid": a[0],
-    "docrev": a[1]
-  }
-  ret = rest.whisk_invoke("%s/delete-document" % db, params)
+def insert(args, id=None):
+  doc = args.copy()
+  doc["type"] = dtype
+  if id:
+    doc["_id"] = id
+  ret = rest.whisk_invoke("%s/create-document" % db, {"doc": doc})
+  if "ok" in ret:
+    return "%s:%s" % (ret["id"], ret["rev"])
+  return None
+
+def find(id=None):
+  query = { "selector": {"type": dtype} }
+  if id:
+    query["selector"]["_id"] = id.split(":")[0]
+  ret = rest.whisk_invoke("%s/exec-query-find" % db, {"query": query})
+  for rec in ret["docs"]:
+    rec["id"] = "%s:%s" % (rec["_id"], rec["_rev"])
   return ret
 
 def update(args):
@@ -30,23 +35,23 @@ def update(args):
   doc["_rev"] = a[1]
   doc["type"] = dtype
   ret = rest.whisk_invoke("%s/update-document" % db, {"doc": doc})
-  return ret
+  if "ok" in ret:
+    return "%s:%s" % (ret["id"], ret["rev"])
+  return None
 
-def insert(args, id=None):
-  doc = args.copy()
-  doc["type"] = dtype
-  if id:
-    doc["_id"] = id
-  ret = rest.whisk_invoke("%s/create-document" % db, {"doc": doc})
-  return ret
-
-def find(id=None):
-  query = { "selector": {"type": dtype} }
-  if id:
-    query["selector"]["_id"] = id.split(":")[0]
-  ret = rest.whisk_invoke("%s/exec-query-find" % db, {"query": query})
-  for rec in ret["docs"]:
-    rec["id"] = "%s:%s" % (rec["_id"], rec["_rev"])
+def delete(id):
+  a = id.split(":")
+  if len(a) == 1:                                 
+    res = find(id) 
+    if res["docs"]:
+      a = res["docs"][0]["id"].split(":")
+    else:
+     return {"error": "not found" }
+  params = { 
+    "docid": a[0],
+    "docrev": a[1]
+  }
+  ret = rest.whisk_invoke("%s/delete-document" % db, params)
   return ret
 
 # import importlib ; importlib.reload(rest);importlib.reload(crud) ; crud.init("demodb","test")
@@ -61,12 +66,10 @@ def test():
     >>> rec = res["docs"][0]
     >>> print(rec["name"])
     Mike
-    >>> id = rec["id"]
     >>> rec["name"] = "Michele"
-    >>> x = crud.update(rec)
+    >>> id1 = crud.update(rec)
     >>> res = crud.find()
     >>> fnd = res["docs"][0]
-    >>> id = fnd["id"]
     >>> print(fnd["name"])
     Michele
     >>> args = {"name":"Miri","email":"miri@sc.com"}
@@ -76,7 +79,7 @@ def test():
     >>> snd = res["docs"][0]
     >>> print(snd["name"])
     Miri
-    >>> x = crud.delete(id) 
+    >>> x = crud.delete(id1) 
     >>> x = crud.delete(nid)
     >>> res = crud.find()
     >>> print(res["docs"])
